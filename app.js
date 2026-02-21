556903d3b1b83dc3b9b9aa894be7fb542def50b1
mahnoorshaikh608 <mahnoorshaikh608@gmail.com>
2026-02-21 14:06:35 +0500

    assignment  1 completed

---
+++ app.js
@@ -0,0 +1,16 @@
+// question no 1
+// alert("hello")
+
+// question no 2
+// alert("Error! Please enter a valid password")
+
+// question no 3
+// alert("Welcome to Js land \n Happy coding!")
+
+// // question no 4
+// alert("Welcome to js land....")
+// alert("Happy coding")
+
+// question no 5 
+// console.log("Hello....I can run JS through my browser's console");
+// alert("Hello....I can run JS through my browser's console")                                                                                          
