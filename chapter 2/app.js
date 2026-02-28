930e1576f19bd5ef9d6eb24ff9ab84ce9828da7b
mahnoorshaikh608 <mahnoorshaikh608@gmail.com>
2026-02-28 14:01:11 +0500

    push

---
+++ chapter 2/app.js
@@ -0,0 +1,39 @@
+// question 1
+
+// let userName;
+
+// question 2
+
+// let myName = "mahnoor Shaikh";
+
+// question 3
+
+// a
+
+// let message
+
+// b
+
+// message="hello world"
+
+// c
+
+// alert(message)
+
+// question 4 
+
+// var studentName = "mahnoor shaikh"
+// var studentAge = 18
+// var studentCourse = "modern web and app development"
+
+// alert(studentName)
+// alert(studentAge)
+// alert(studentCourse)
+
+
+// question 5
+// var pizza = "PIZZA"
+
+// question 6 
+
+
