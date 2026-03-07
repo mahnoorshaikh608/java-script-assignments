ddb7e1839da8941e575f2fae1ad74924ee464977
sabasohail472 <sabasohail472@gmail.com>
2026-03-07 14:01:16 +0500

    chapter 5

---
+++ chapter5/app.js
@@ -0,0 +1,67 @@
+// question 1
+// let num1 = +prompt("enter your first number")
+// let num2 = +prompt("enter your second number")
+// let sum = (num1 + num2)
+
+// document.write(`sum of ${num1} and ${num2}is ${sum} `)
+
+// question 2
+// a
+// let num1 = +prompt("enter your first number")
+// let num2 = +prompt("enter your second number")
+// let sum = num1 - num2
+
+// document.write(`sum of ${num1} and ${num2} is ${sum} `)
+// b 
+// let num1 = +prompt("enter your first number")
+// let num2 = +prompt("enter your second number")
+// let sum = num1 * num2
+
+// document.write(`sum of ${num1} and ${num2} is ${sum} `)
+// c
+// let num1 = +prompt("enter your first number")
+// let num2 = +prompt("enter your second number")
+// let sum = num1 / num2
+
+// document.write(`sum of ${num1} and ${num2} is ${sum} `)
+
+// question 3 
+// a
+// let num
+
+// b
+// document.write(`value after declaration is ${num}`)
+// document.write(<br/>)
+// c
+
+// num = 5
+
+// d
+
+// document.write(`Initial value is ${num}`)
+
+// e
+num++
+
+// f
+
+// document.write(`value after increment is ${num}`)
+
+// g
+
+num = num + 7
+
+// h
+
+// document.write(`value after addition is ${num}`)
+
+// i
+
+num--
+
+// j
+document.write(`value after decrement is ${num}`)
+
+// question 4 
+
+
